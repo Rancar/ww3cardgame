f6c15f4be01a3e7d02b4f5af71fdfb92fee25f59
Rancar <paddyo@gmail.com>
2019-02-03 09:55:23 -0800

    DevPeekLook Initial Unity Project
    Initial Unity Project with look and feel only.

---
+++ Assets/Scripts/CardModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardModel : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    public Sprite[] faces;
+    public Sprite cardBack;
+    public int cardIndex;
+    public int[] health;
+    public int[] armor;
+    public int[] heAttack;
+    public int[] armorAttack;
+    public int[] oilCost;
+    public int[] heartCost;
+    public int[] movementSpeed;
+    public string[] cardText;
+    public string[] cardName;
+
+    public void ToggleFace(bool showFace)
+    {
+        if (showFace)
+        {
+            spriteRenderer.sprite = faces[cardIndex];
+        }
+        else
+        {
+            spriteRenderer.sprite = cardBack;
+        }
+    }
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
