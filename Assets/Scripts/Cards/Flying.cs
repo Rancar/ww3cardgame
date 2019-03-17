6613c580f0b86ccf381ed5794f6776f42128a794
Rancar <paddyo@gmail.com>
2019-03-17 10:40:34 -0700

    Added Flying card type
    Added the flying card type and an example Spitfire MK.2 card

---
+++ Assets/Scripts/Cards/Flying.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(menuName = "Cards/Flying")]
+public class Flying : CardType
+{
+    public override void OnSetType(CardViz viz)
+    {
+        base.OnSetType(viz);
+    }
+}
